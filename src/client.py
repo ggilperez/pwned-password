11839d00f064ef3381aa23ea4b76ffb1ca1c5302
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-05-21 01:22:56 +0200

    create client

---
+++ src/client.py
@@ -0,0 +1,5 @@
+from urllib import request
+response = request.urlopen("http://localhost:8000/")
+print(f"STATUS: {response.status}")
+print(f"HEADERS:\r\n{response.headers}")
+print(f"DATA: {response.read()}")
