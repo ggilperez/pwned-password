fc6721b85281adc2b865f027fb271107571a9e11
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-05-21 17:18:28 +0200

    create config file

---
+++ configs/base.py
@@ -0,0 +1,10 @@
+config = {
+    "url": "https://api.pwnedpasswords.com/range",
+    "schema": "http",
+    "host": "0.0.0.0",
+    "port": 8000,
+    "param": "password",
+    "encoding": "UTF-8",
+    "timeout": 10,
+    "prefix_index": 5,
+}
